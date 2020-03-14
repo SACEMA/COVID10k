84f2bf16e9990d0a7dbf8c7e0245efba8451d999
Carl A. B. Pearson <blank101@gmail.com>
2020-03-14 16:28:12 -0400

    figure generation updates

--- Makefile
+++ Makefile
@@ -7,6 +7,12 @@ R = Rscript $^ $@
 bpsamples-%.rds: bpsamples.R params.json
 	time ${R}
 
+# use to create digests
+digested.rds: digest.R $(wildcard bpsamples-*.rds)
+	${R}
+
+distros.rds quantiles.rds: digested.rds 
+
 # use the branching samples to estimate...TBD
-estimates.png: estimate.R bpsamples.rds
+estimates.png: estimate.R digested.rds distros.rds quantiles.rds
 	${R}
---
+++ digest.R
@@ -0,0 +1,35 @@
+suppressPackageStartupMessages({
+  require(data.table)
+})
+
+.args <- c(list.files(pattern = "^bpsamples-.+rds$"), "digest.rds")
+.args <- commandArgs(trailingOnly = TRUE)
+
+.sampfls <- head(.args, -1)
+.sample_offset <- readRDS(.sampfls[1])[, max(sample_id)+1]
+
+.res <- rbindlist(lapply(seq_along(.sampfls), function(ind) {
+  readRDS(.sampfls[ind])[,
+    .SD[1:which.max(cumcase > 1e4)][, .(day, cumcase=c(cumcase[-.N], 1e4))],
+    by=.(sample_id = sample_id + (ind-1)*.sample_offset)
+  ]
+}))
+
+lines.dt <- copy(.res)[, .(
+  measure = "cumcases", value = cumcase, day, sample_id
+)]
+
+bars.dt <- rbind(
+  .res[, .(day=day[.N], value=1e4), by=sample_id],
+  .res[, .(day=day[which.max(cumcase > 1e3)], value=1e3), by=sample_id]
+)[, measure := "distribution" ]
+
+qs.dt <- bars.dt[,{
+  qs <- quantile(day, probs = c(0.025, 0.25, 0.5, 0.75, 0.975))
+  names(qs) <- c("lo.lo","lo","med","hi","hi.hi")
+  c(as.list(qs), measure = "cumcases")
+}, by=value]
+
+saveRDS(lines.dt, tail(.args, 1))
+saveRDS(bars.dt, gsub("(/)?\\w+\\.rds","\\1distros.rds",tail(.args, 1)))
+saveRDS(qs.dt, gsub("(/)?\\w+\\.rds","\\1quantiles.rds",tail(.args, 1)))
