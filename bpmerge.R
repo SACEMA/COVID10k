b2dbae391725fae86add1c5dff63ffb199913cab
Carl A. B. Pearson <blank101@gmail.com>
2020-03-29 18:13:53 -0400

    merge branching process results

---
+++ bpmerge.R
@@ -0,0 +1,36 @@
+suppressPackageStartupMessages({
+  require(data.table)
+  require(jsonlite)
+})
+
+.args <- if (interactive()) c(
+  "/Users/carlpearson/Dropbox/COVIDSA/outputs/params",
+  "/Users/carlpearson/Dropbox/COVIDSA/outputs/bpsR2/bpmerge.rds"
+) else commandArgs(trailingOnly = TRUE)
+
+parsfls <- list.files(.args[1], "\\.json", full.names = TRUE)
+pth <- dirname(.args[2])
+tar <- basename(.args[2])
+
+fns <- grep(tar,
+  list.files(pth, "\\.rds"),
+  invert = TRUE, fixed = TRUE, value = TRUE
+)
+
+ctys <- gsub("(\\w+)-.+","\\1",fns)
+pars <- grep(paste(ctys, collapse = "|"), parsfls, value = T)
+
+res <- setkey(rbindlist(mapply(function(fn, par) {
+  res <- readRDS(sprintf("%s/%s",pth,fn))[,
+    country := gsub("(\\w+)-.+","\\1",basename(fn))
+  ]
+  p <- read_json(par)
+  res[,
+    date := day + as.Date(p$day0)
+  ][,
+    .(cumcase),
+    keyby=.(country, sample_id, date)
+  ]
+}, fn = fns, par = pars, SIMPLIFY = FALSE)), country, sample_id, date)
+
+saveRDS(res, tail(.args, 1))
